d4ac60b91d1dbcb08779895b192e2187128f8f78
Your Name <you@example.com>
2022-11-26 20:19:25 +0500

    Commit

---
+++ app/src/main/java/com/example/aaa4160/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aaa4160;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
